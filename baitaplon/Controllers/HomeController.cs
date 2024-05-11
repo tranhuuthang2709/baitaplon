4dcbab490ed098b27a6b36da93bc227c7b79d32b
tranhuuthang2709 <112497755+tranhuuthang2709@users.noreply.github.com>
2024-05-11 16:31:18 +0700

    Add project files.

---
+++ baitaplon/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace baitaplon.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+     public ActionResult gioithieu()
+        {
+            return View();
+        }
+        public ActionResult tintuc()
+        {
+            return View();
+        }
+    }
+}
