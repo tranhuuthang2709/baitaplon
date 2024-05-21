834504c6c56516c7c0c5ab19d474ba4748daae77
tranhuuthang2709 <112497755+tranhuuthang2709@users.noreply.github.com>
2024-05-21 23:49:06 +0700

    create function manage

---
+++ baitaplon/Areas/Admin/Controllers/HomemanageController.cs
@@ -0,0 +1,56 @@
+﻿using baitaplon.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace baitaplon.Areas.Admin.Controllers
+{
+    public class HomemanageController : Controller
+    {
+        QLSVEntities db = new QLSVEntities();
+        // GET: Admin/Home
+        public ActionResult Index()
+        {
+            string name = Convert.ToString(Session["name"]);
+            ViewBag.name = name;
+            return View();
+        }
+        [HttpGet]
+        public ActionResult loginadmin()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult loginadmin(string Username, string Password)
+        {
+            if (ModelState.IsValid)
+            {
+                var acc = db.Admins.SingleOrDefault(x => x.Username == Username && x.Password == Password);
+                if (acc != null)
+                {
+                    Session["user"] = acc;
+                    Session["name"] = acc.Username.ToString();
+                    return RedirectToAction("Index", "Homemanage");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Thông tin đăng nhập không đúng.");
+                    return View();
+                }
+            }
+            else
+            {
+                return View();
+            }
+        }
+        public ActionResult logout()
+        {
+            Session.Clear();
+            return RedirectToAction("Index", "Home", new { area = "" });
+        }
+
+    }
+}
---
+++ baitaplon/Areas/Admin/Controllers/SinhVienmanageController.cs
@@ -0,0 +1,84 @@
+﻿using baitaplon.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace baitaplon.Areas.Admin.Controllers
+{
+    public class SinhVienmanageController : Controller
+    {
+        // GET: Admin/SinhVien
+
+        QLSVEntities db = new QLSVEntities();
+        public ActionResult Index()
+        {
+            var listsinhvien = db.sinhvien.ToList();
+            return View(listsinhvien);
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(sinhvien model)
+        {
+            if (ModelState.IsValid)
+            {
+                db.sinhvien.Add(model);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(model);
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int MaSV)
+        {
+            var sinhvienmodel = db.sinhvien.Find(MaSV);
+            if (sinhvienmodel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sinhvienmodel);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(sinhvien model)
+        {
+            if (ModelState.IsValid)
+            {
+                var sv = db.sinhvien.Find(model.MaSV);
+                if (sv != null)
+                {
+                    sv.TenSV = model.TenSV;
+                    sv.MatKhau = model.MatKhau;
+                    sv.GioiTinh = model.GioiTinh;
+                    sv.SDT = model.SDT;
+                    sv.NgaySinh = model.NgaySinh;
+                    sv.MaLop = model.MaLop;
+
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    return HttpNotFound();
+                }
+            }
+            return View(model);
+        }
+
+        public ActionResult Delete(int MaSV)
+        {
+            var model = db.sinhvien.Find(MaSV);
+            db.sinhvien.Remove(model);
+            db.SaveChanges();
+            return RedirectToAction("index");
+        }
+       
+    }
+}
